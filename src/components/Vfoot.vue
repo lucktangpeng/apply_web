3714078323023b6ae7dfd843bd07a04fe96c358f
唐鹏 <961624856@qq.com>
2019-05-11 22:50:49 +0800

    验证码验证和数据提交完成

--- src/components/Vfoot.vue
+++ src/components/Vfoot.vue
@@ -7,11 +7,12 @@
     </div>
 </template>
 <script>
+import Cookie from 'vue-cookies'
 export default {
     name:"Vhear",
     data(){
         return{
-
+            code_status:""
         }
     },
     props:{
@@ -24,15 +25,83 @@ export default {
     },
     methods:{
         click_code(){
-            console.log(this.sendcode)
-            console.log("所有的数据")
-            console.log(this.course_id)
-            console.log(this.ph_code)
-            console.log(this.company)
-            console.log(this.area)
-            console.log(this.agent_id)
-            this.$store.dispatch("code_aox",{"phone":this.ph_code,"code":this.sendcode})
+            // console.log(this.sendcode)
+            // console.log("所有的数据")
+            // console.log(this.course_id)
+            // console.log(this.ph_code)
+            // console.log(this.company)
+            // console.log(this.area)
+            // console.log(this.agent_id)
+           
+            console.log("测试异步pro")
 
+            // this.$store.dispatch("code_aox",{"phone":this.ph_code,"code":this.sendcode})
+            var that = this
+            this.$axios.request({
+            url:"http://127.0.0.1:8000/api/phone/",
+            method:"PUT",
+            data:{"phone":this.ph_code,"code":this.sendcode},
+            headers:{
+                'Content-Type':'application/json',
+            }
+            }).then(function(date){
+            // 请求发送成功
+                console.log("发送验证码的请求")
+                console.log(date.data.status)
+                that.$store.state.code_error = date.data
+                // console.log(this)
+                // that.code_status = date.data.status
+                if(date.data.status){
+                    that.$axios.request({
+                        url:"http://127.0.0.1:8000/api/record/",
+                        method:"POST",
+                        data:{"phone":that.ph_code,
+                                "company":that.company,
+                                "agent_id":that.agent_id,
+                                "area":that.area,
+                                "course_id":that.course_id,},
+                        headers:{
+                        'Content-Type':'application/json',
+                        }
+                    }).then(function(date){
+                        // 请求发送成功
+                    console.log("发送全部数据的请求")
+                    console.log(date)
+                    // context.commit("RECORD_VALUES",date)
+                        that.$store.state.code_error = ""
+                    }).catch(function(){
+                        // 请求发送失败
+                        console.log("数据提交请求失败111")
+                    })
+                }
+                
+                
+            // context.commit('CODE_AOX',date)
+            }).catch(function(){
+            // 请求发送失败
+            console.log("请求失败11")
+            })
+             console.log(this.code_status)
+            
+
+             
+            // console.log(Cookie.get("code_status"))
+            // if(Cookie.get("code_status")){
+            //     this.$store.dispatch("record_values",{"phone":this.ph_code,
+            //                                           "company":this.company,
+            //                                           "agent_id":this.agent_id,
+            //                                           "area":this.area,
+            //                                           "course_id":this.course_id,
+            //                                           })
+
+           
+            // }
+
+        }
+    },
+    computed:{
+        code_err(){
+            return this.$store.state.code_error
         }
     }
 }
--- src/store/store.js
+++ src/store/store.js
@@ -10,6 +10,7 @@ export default new Vuex.Store({
         error:"",
         code_error:"",
         phone_statu:"",
+        recprd_status:"",
     },
     mutations: {
       COURSE_AOX (state,date) {
@@ -21,15 +22,12 @@ export default new Vuex.Store({
          console.log(state.error)
         
       },
-      CODE_AOX (state,date) {
-          state.code_error = date.data
-          console.log(state.code_error)
-      }
+  
     },
     actions:{
       course_aox (context) {
         Vue.prototype.$axios.request({
-          url:"http://192.168.10.195:8000/api/course/",
+          url:"http://127.0.0.1:8000/api/course/",
           method:"GET",
           headers:{
             'Content-Type':'application/json',
@@ -46,7 +44,7 @@ export default new Vuex.Store({
       },
       phone_aox (context,date) {
         Vue.prototype.$axios.request({
-          url:"http://192.168.10.195:8000/api/phone/",
+          url:"http://127.0.0.1:8000/api/phone/",
           method:"POST",
           data:{"phone":date},
           headers:{
@@ -62,23 +60,43 @@ export default new Vuex.Store({
         })
         
       },
-      code_aox (context,date) {
-        Vue.prototype.$axios.request({
-          url:"http://192.168.10.195:8000/api/phone/",
-          method:"PUT",
-          data:date,
-          headers:{
-            'Content-Type':'application/json',
-          }
-        }).then(function(date){
-          // 请求发送成功
-          console.log(date.data)
-          context.commit('CODE_AOX',date)
-        }).catch(function(){
-          // 请求发送失败
-          console.log("请求失败")
-        })
-        
-      },
+      
+    // record_values(context,date){
+    //   Vue.prototype.$axios.request({
+    //     url:"http://127.0.0.1:8000/api/record/",
+    //     method:"POST",
+    //     data:date,
+    //     headers:{
+    //       'Content-Type':'application/json',
+    //     }
+    //   }).then(function(date){
+    //     // 请求发送成功
+    //     console.log(date.data)
+    //     context.commit('RECORD_VALUES',date)
+    //   }).catch(function(){
+    //     // 请求发送失败
+    //     console.log("数据提交请求失败")
+    //   })
+    // },
+    // record_values(context,date){
+    //   Vue.prototype.$axios.request({
+    //         url:"http://127.0.0.1:8000/api/record/",
+    //         method:"POST",
+    //         data:date,
+    //         headers:{
+    //           'Content-Type':'application/json',
+    //         }
+    //       }).then(function(date){
+    //         // 请求发送成功
+    //        console.log("发送全部数据的请求")
+    //        console.log(date)
+    //        context.commit("RECORD_VALUES",date)
+            
+    //       }).catch(function(){
+    //         // 请求发送失败
+    //         console.log("数据提交请求失败111")
+    //       })
+
+    // },
     }
   })
